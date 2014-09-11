f4fdb0753e642f5603ae67159f476780a0800fbb
osvaldomiranda <osvaldo.omiranda@gmail.com>
2014-09-11 19:04:35 -0400

    [CREA Maqueta Clase Proceso IAT]

---
+++ IatDteBridge/ProcesoIat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IatDteBridge
+{
+    class ProcesoIat
+    {
+
+        /*
+         * Implementar el siguiente ciclo en un Thread con un ciclo iterativo
+         */
+
+        // Instancia txt_admin
+
+        // Ejecuta metodo de txt_admin que llena y obtienen Clase Documento
+ 
+        // intancia objeto de la clase PDF_admin
+
+        // ejecutar metodo de PDF_admin que recibe objeto de la clase Documento
+        // que genera el archivo pdf
+
+        //instancia Clase de tipo Impresora
+
+        // Ejecuta metodo que recibe archivo pdf y lo imprime
+
+
+        // instancia XML_admin
+
+        // Ejecuta metodo de XML_admin que recibe objeto de la clase documento 
+        // que llena el xml lo firma, lo timbra y devuelve la factura xml lista
+
+
+        // instancia objeto de tipo Connect
+
+        // ejecuta metodo de Connect que recibe el xml y lo envía al Core
+
+
+        // Continuar con siguiente documento
+
+
+
+    }
+}
