369f149d7c0e219ceabe623b0ac9981a06c75abe
mjimenez2014 <jimenezmaury@gmail.com>
2014-11-19 15:33:36 -0300

    [FixLibGuia]

--- IatDteBridge/xmlLibroCompra.cs
+++ IatDteBridge/xmlLibroCompra.cs
@@ -28,6 +28,7 @@ namespace IatDteBridge
                 + "<TipoOperacion>"+libro.TipoOperacion+"</TipoOperacion>\n"
                 + "<TipoLibro>"+libro.TipoLibro+"</TipoLibro>\n"
                 + "<TipoEnvio>"+libro.TipoEnvio+"</TipoEnvio>\n"
+                + "<FolioNotificacion>" + libro.FolioNotificacion + "</FolioNotificacion>\n"
               + "</Caratula>\n";
 
 
@@ -115,6 +116,10 @@ namespace IatDteBridge
             {   
                 if(detIvaNo == null)
                     detIvaNo ="";
+
+                String mntiva = "<MntIVA>" + det.MntIVA+ "</MntIVA>\n";
+                if(det.MntIVA == 0)
+                    mntiva = "";
                 detalle = "<Detalle>\n" +
                   "<TpoDoc>" + det.TpoDoc + "</TpoDoc>\n" +
                   "<NroDoc>" + det.NroDoc + "</NroDoc>\n" +
@@ -125,7 +130,7 @@ namespace IatDteBridge
                   "<RznSoc>" + det.RznSoc + "</RznSoc>\n" +
                   "<MntExe>" + det.MntExe + "</MntExe>\n" +
                   "<MntNeto>" + det.MntNeto + "</MntNeto>\n"+ 
-                  "<MntIVA>" + det.MntIVA+ "</MntIVA>\n";
+                  mntiva;
                  if (det.IVANoRec == null)
                  {
                      detIvaNo = "";
